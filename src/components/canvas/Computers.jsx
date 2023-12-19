56ab607128cab0cfff5898c2c4ee4c0c381369ca
tjay1760 <mcjthiongo@gmail.com>
2023-12-19 10:40:37 +0300

    Add hero model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,9 +7,22 @@ const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
 <mesh>
-  <hemisphereLight intensity={0.15} groundColor={black}/>
+  <hemisphereLight intensity={0.15} groundColor="black"/>
   <pointLight intensity={1}/>
-  <primitive object={computer.scene}/>
+  <spotLight 
+  position={[-2,50,10]}
+  angle={0.12}
+  penumbra={1}
+  intensity ={1}
+  castShadow
+  shadow-mapsize = {1024}
+  />
+  <primitive 
+  object={computer.scene}
+  scale ={0.75}
+  position = {[0,-3.25,-1.5]}
+  rotation = {[-0.01, -0.2, -0.01]}
+  />
 </mesh>
   )
 }
@@ -21,7 +34,7 @@ const ComputersCanvas =() =>{
     camera={{position:[20,3,5], fov: 25}}
     gl={{preserveDrawingBuffer:true}}
     >
-      <Suspense fallback={<CanvasLoader/>}>
+      <Suspense>
         <OrbitControls enableZoom ={false}
         maxPolarAngle={Math.PI/2}
         minPolarAngle={Math.PI/2}
@@ -33,4 +46,4 @@ const ComputersCanvas =() =>{
   )
 }
 
-export default Computers
+export default ComputersCanvas
