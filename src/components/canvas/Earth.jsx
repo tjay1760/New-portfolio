c6c8752244f4a7f72713159d7b0f5d3ff22d7813
tjay1760 <mcjthiongo@gmail.com>
2023-12-21 00:09:24 +0300

    Earth model Aded

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -7,6 +7,9 @@ const Earth = () => {
   return (
 <primitive
 object={earth.scene}
+scale = {2.5}
+position-y ={0}
+rotation-y ={0}
 />
   )
 }
@@ -16,7 +19,12 @@ const EarthCanvas = ()=>{
     shadows
     frameloop ='demand'
     gl = {{preserveDrawingBuffer:true}}
-    camera ={{}}
+    camera ={{
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [-4,3,6]
+    }}
     >
 <Suspense fallback={<CanvasLoader/>}>
   <OrbitControls
