3a3cfe2139fa8f34574294bce4884faa5e9c8b1c
tjay1760 <mcjthiongo@gmail.com>
2023-12-20 18:58:05 +0300

    add Testimonials

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -6,8 +6,32 @@ import { fadeIn, textVariant } from '../utils/motion'
 import { testimonials } from '../constants'
 
 const FeedbackCard =({index,testimonial,name,designation,company,image})=>(
-<motion.div>
-  <p>{testimonial}</p>
+<motion.div
+variants={fadeIn("", "spring", index*0.5, 0.75)}
+className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+>
+  <p className='text-white font-black text-[48px]'>&quot;</p>
+  <div className='mt-1'>
+  <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+  <div
+  className='mt-7 flex justify-between items-center gap-1'
+  >
+    <div className='flex flex-col flex-1'>
+      <p className="text-white font-medium text-[16px]">
+        <span className='blue-text-gradient'>@</span>{name}
+      </p>
+      <p className='mt-1 text-secondary text-[12px]'>
+        {designation} of {company}
+      </p>
+      </div> 
+      <img
+      src= {image}
+      alt = {`feedback-by-${name}`}
+      className = "w-10 h-10 rounded-full object-cover"
+      />
+  </div>
+  </div>
+  
 </motion.div>
 )
 
@@ -31,4 +55,4 @@ const Feedbacks = () => {
   )
 }
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, "") 
