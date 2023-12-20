63fd5d41060713ce898a1bf2eee722ba2fb42571
tjay1760 <mcjthiongo@gmail.com>
2023-12-20 23:23:38 +0300

    Contact form design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,6 +26,55 @@ message:""
       >
 <p className={styles.sectionSubText}>Get in Touch</p>
 <h3 className={styles.sectionHeadText}>Contact.</h3>
+<form
+// ref={formRef}
+onSubmit = {handleSubmit}
+classname = 'mt-12 flex flex-col gap-8'
+>
+<label className= "flex flex-col">
+  <span className="text-white font-medium mb-4">Your Name: </span>
+  <input 
+  type="text"
+  name="name" 
+  value={form.name} 
+  placeholder='What is your name?'
+  className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
+  />
+</label>
+<label className= "flex flex-col">
+  <span className="text-white font-medium mb-4">Your Email: </span>
+  <input 
+  type="email"
+  name="email" 
+  value={form.email} 
+  placeholder='What is your email?'
+  className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
+  />
+</label>
+<label className= "flex flex-col">
+  <span className="text-white font-medium mb-4">Your Name: </span>
+  <textarea
+  rows ='7' 
+  name="message" 
+  value={form.message} 
+  placeholder='What do yo want to say?'
+  className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
+  />
+</label>
+<button
+type='submit'
+className='text-white mt-5 bg-tertiary py-3 px-8 outline-none w-fit font-bold shadow-md shadow-primary border-none rounded-xl'
+>
+  {loading? "Sending" : "Send"}
+</button>
+</form>
+      </motion.div>
+      <motion.div
+      variants ={slideIn('right','tween',0.2,1)}
+      className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas/>
+
       </motion.div>
 
     </div>
