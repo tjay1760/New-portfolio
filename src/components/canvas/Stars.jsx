4367073f0ad410d1ee6e50cf526550a0a12eb4a5
tjay1760 <mcjthiongo@gmail.com>
2023-12-21 00:19:54 +0300

    start canvas init

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
-const Stars = () => {
+import {useState, useRef, Suspense} from 'react'
+import { Canvas,useFrame } from '@react-three/fiber'
+import { Point, PointMaterial, Preload } from '@react-three/drei'
+import * as random from 'maath/random/dist/maath-random.esm'
+const Stars = (props) => {
   return (
     <div>Stars</div>
   )
 }
+const StarsCanvas = () => {}
 
-export default Stars
+export default StarsCanvas;
