f63e83cad1a624630ab10bca678eb25a0fd45370
tjay1760 <mcjthiongo@gmail.com>
2023-12-20 21:00:41 +0300

    contact form boilerplate

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState, useRef } from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../style'
+import { SectionWrapper } from '../hoc'
+import { slideIn } from '../utils/motion'
+import { EarthCanvas } from './canvas'
+// import {emaijs} from '@emailjs/browser'
 
 const Contact = () => {
+  const formRef = useRef;
+  const [form,setForm] = useState({
+name:"",
+email:"",
+message:""
+  });
+  const [loading, isLoading] = useState(false);
+  const handleChange=()=>{
+  }
+  const handleSubmit =()=>{}
   return (
-    <div>Contact</div>
+    <div
+    className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div
+      variants ={slideIn('left','tween',0.2,1)}
+      className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+<p className={styles.sectionSubText}>Get in Touch</p>
+<h3 className={styles.sectionHeadText}>Contact.</h3>
+      </motion.div>
+
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact,"contact")
