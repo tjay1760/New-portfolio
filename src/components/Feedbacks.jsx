d079ef8c229d7bfa992a098716798d2150eee302
tjay1760 <mcjthiongo@gmail.com>
2023-12-20 17:51:32 +0300

    render testimonial

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../style'
+import { SectionWrapper } from '../hoc'
+import { fadeIn, textVariant } from '../utils/motion'
+import { testimonials } from '../constants'
+
+const FeedbackCard =({index,testimonial,name,designation,company,image})=>(
+<motion.div>
+  <p>{testimonial}</p>
+</motion.div>
+)
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+<div className={`${styles.paddingX} bg-tertiary rounded-2xl min-h-[300px]`}>
+<motion.div variants={textVariant()}>
+<p className={styles.sectionSubText}>What others say ...</p>
+<h2 className={styles.sectionHeadText}>Testimonials.</h2>
+</motion.div>
+</div>
+<div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+{testimonials.map(
+  (testimonial, index)=>(
+    <FeedbackCard key={testimonial.name} index={index}{...testimonial}/>
+  )
+)}
+</div>
+    </div>
   )
 }
 
