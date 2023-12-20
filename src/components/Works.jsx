2e2ea2a8dbc807f40d67d89c35b9b37e3619dfeb
tjay1760 <mcjthiongo@gmail.com>
2023-12-20 14:33:42 +0300

    assets import

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import {Tilt} from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../style'
+import { github } from '../assets'
+import { SectionWrapper } from '../hoc'
+import { projects } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
 
 const Works = () => {
   return (
